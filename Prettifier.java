7d4b3f7c259d87a6a67320f816a42d39bc600480
Andero <andero.sc@gmail.com>
2025-03-17 22:00:16 +0200

    date and time modifing

--- Prettifier.java
+++ Prettifier.java
@@ -12,23 +12,80 @@ public class Prettifier {
             while (myReader.hasNextLine()){
                 String data = myReader.nextLine();
 
-                if(data.contains("T12")){
-                    System.out.println("yess");
-                    // System.out.println(data);
+                int number =  data.indexOf("T12");
+                String date = data.substring(number +4, (data.length() - 1));
 
-                    int number =  data.indexOf("T12");
-                    String date = data.substring(number +4, (data.length() - 1));
-                    System.out.println(date);
+                if(data.contains("T12")){
+                    // System.out.println(data);
+                    // System.out.println(date);
+                    String time = date.substring(11, 16);
+
+                    int hours = Integer.parseInt(date.substring(11, 13));
+                    String minutes = date.substring(14, 16);
+                    String hoursWithEnding = "";
+                    // System.out.println(time);
+                    if(hours > 12){
+                        hours = hours - 12;
+                        hoursWithEnding = "0" + hours + ":" + minutes+ "PM";
+                    } else{
+                        hoursWithEnding = "0" + hours + ":" + minutes+ "AM";
+                    }
+
+                    // System.out.println(hoursWithEnding);
+
+                    String offset = "";  
+                    String lastChar = date.substring(date.length()-1);
+
+
+                    if(lastChar.equals("Z")){
+                        offset = "+00:00";
+                    } else if(lastChar != "Z"){
+                        offset = date.substring( date.length() -6, date.length());
+                    }
+
+                    String answer = hoursWithEnding + " " + "(" + offset + ")";
+                    System.out.println(answer);
+
 
 
                 }else if (data.contains("T24")) {
-                    System.out.println("yess");
-                    int number =  data.indexOf("T24");
-                    String date = data.substring(number +4, (data.length() - 1));
+                    // System.out.println(data);
+                    // System.out.println(date);
+
+                    String hours = date.substring(15, 17);
+                    String minutes = date.substring(18, 20);
+                    String hoursWithEnding = "";
+                    // System.out.println(time);
+
+
+                        hoursWithEnding = hours + ":" + minutes+ "PM";
+
+                        hoursWithEnding =  hours + ":" + minutes+ "AM";
+
+                    // System.out.println(hoursWithEnding);
+
+                    String offset = "";  
+                    String lastChar = date.substring(date.length()-1);
+
+
+                    if(lastChar.equals("Z")){
+                        offset = "+00:00";
+                    } else if(lastChar != "Z"){
+                        offset = date.substring( date.length() -6, date.length());
+                    }
+
+                    String answer = hoursWithEnding + " " + "(" + offset + ")";
+                    System.out.println(answer);
+
+                }else if(data.contains("D(")){
                     System.out.println(date);
+                    String day = date.substring(date.length());
+                    String month = "";
+                    String year = "";
+                    
+
                 }else{
                     System.out.println("dont have");
-
 
                 }
 
@@ -37,10 +94,6 @@ public class Prettifier {
         } catch (FileNotFoundException e){
             System.out.println("errrrror");
         }
-    }
-
-    public static void date(String isodate){
-
     }
 
 }
